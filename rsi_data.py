11a21cc86c10b3c6341692f6249759a5f142b222
Alex97675 <bitcoin.tulgaa@gmail.com>
2026-09-08 15:28:42 +0800

    Add rsi_array to return data structure

--- rsi_data.py
+++ rsi_data.py
@@ -111,6 +111,7 @@ def calculate_rsi_report(klines, symbol="UNKNOWN"):
 
         return {
             "symbol": symbol,
+            "rsi_array": [ ... ],
             "values": {"0": rsi0, "-1": rsi1, "-2": rsi2, "-3": rsi3},
             "last_status": last_status,
             "trend": trend,
