ae12178084ba2d49c6b9be4c19e0155b0a09b76f
Alex97675 <bitcoin.tulgaa@gmail.com>
2026-09-11 15:13:11 +0800

    Update timestamp formatting for Ulaanbaatar timezone
    Refactored timestamp formatting to use Ulaanbaatar timezone.

--- data_topmovers.py
+++ data_topmovers.py
@@ -1,24 +1,11 @@
 import pandas as pd
 from datetime import datetime, timezone, timedelta
 
-# Улаанбаатарын цагийн бүс (UTC+8)
-ub_timezone = timezone(timedelta(hours=8))
-
-formatted_time = ""
-if active_timestamp:
-    try:
-        ts = int(active_timestamp)
-        if ts > 10000000000:
-            ts = ts / 1000
-        # UTC цагийг Улаанбаатарын цаг руу шилжүүлэх
-        dt_utc = datetime.fromtimestamp(ts, tz=timezone.utc)
-        dt_ub = dt_utc.astimezone(ub_timezone)
-        formatted_time = dt_ub.strftime('%Y-%m-%d %H:%M:%S')
-    except Exception:
-        formatted_time = str(active_timestamp)
-
 def calculate_top_movers_report(kline_history, cache_lock, macd_state=None):
     movers_list = []
+    
+    # Улаанбаатарын цагийн бүс (UTC+8)
+    ub_timezone = timezone(timedelta(hours=8))
 
     with cache_lock:
         for symbol, klines in kline_history.items():
@@ -74,7 +61,10 @@ def calculate_top_movers_report(kline_history, cache_lock, macd_state=None):
                     ts = int(active_timestamp)
                     if ts > 10000000000:
                         ts = ts / 1000
-                    formatted_time = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                    # UTC цагийг Улаанбаатарын цаг руу шилжүүлэх
+                    dt_utc = datetime.fromtimestamp(ts, tz=timezone.utc)
+                    dt_ub = dt_utc.astimezone(ub_timezone)
+                    formatted_time = dt_ub.strftime('%Y-%m-%d %H:%M:%S')
                 except Exception:
                     formatted_time = str(active_timestamp)
 
