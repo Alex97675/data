74289e8cf8b9690ac8c9c2385f7435879a973e4d
Alex97675 <bitcoin.tulgaa@gmail.com>
2026-09-11 13:51:35 +0800

    Update data.py

--- data.py
+++ data.py
@@ -16,7 +16,7 @@ from ohlc_data import calculate_ohlc_tracker_report
 from rsi_data import calculate_rsi_report
 from ema_data import calculate_ema_report
 from macd_data import calculate_macd_report, _build_initial_macd_state, macd_state
-from data_topmovers import calculate_top_movers_report
+from data_topmovers import calculate_gain_lose_report
 
 # ==================== CONFIG ====================
 MAX_KLINES = 300  # Лааны түүхэн датаны хязгаар
