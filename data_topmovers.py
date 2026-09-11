b7fc1ccece857169d005e81ff4223735f9e72f7e
Alex97675 <bitcoin.tulgaa@gmail.com>
2026-09-11 16:08:10 +0800

    Limit top gainers and losers to 50 each

--- data_topmovers.py
+++ data_topmovers.py
@@ -83,6 +83,6 @@ def calculate_top_movers_report(kline_history, cache_lock, macd_state=None):
     sorted_by_gain = sorted(movers_list, key=lambda x: x["change_percent"], reverse=True)
 
     return {
-        "top_gainers": sorted_by_gain[:500],
-        "top_losers": sorted_by_gain[-500:][::-1]
+        "top_gainers": sorted_by_gain[:50],
+        "top_losers": sorted_by_gain[-50:][::-1]
     }
